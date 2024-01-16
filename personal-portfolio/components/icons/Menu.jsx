258086024888d56e2c915510d7bda778ada229f7
erdenetsogta <a.erdenetsogt@gmail.com>
2024-01-16 19:02:54 +0800

    fix svg props, lg:flex

--- personal-portfolio/components/icons/Menu.jsx
+++ personal-portfolio/components/icons/Menu.jsx
@@ -1,33 +1,9 @@
 export function Menu() {
   return (
-    <svg
-      width="36"
-      height="36"
-      viewBox="0 0 36 36"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M10 18H26"
-        stroke="#4B5563"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-      <path
-        d="M10 12H26"
-        stroke="#4B5563"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-      <path
-        d="M10 24H26"
-        stroke="#4B5563"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
+    <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M10 18H26" stroke="#4B5563" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M10 12H26" stroke="#4B5563" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M10 24H26" stroke="#4B5563" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
   );
 }
--- personal-portfolio/pages/index.js
+++ personal-portfolio/pages/index.js
@@ -1,21 +1,20 @@
 import { Header } from "../components/Header";
 
+const name = "Sagar";
+
 export default function Home() {
   return (
     <div>
       <Header />
-      <div className="px-2">
-        <div className="flex justify-center mt-10">
+      <div className="container px-2 mx-auto lg:flex">
+        <div className="flex justify-center order-2 mt-10">
           <img src="/images/profile.jpg" alt="profile" />
         </div>
-        <div className="mt-20">
-          <h2 className="text-4xl">Hi, I’m Sagar 👋</h2>
-          <p className="text-base mt-2 text-slate-500">
-            I'm a full stack developer (React.js & Node.js) with a focus on
-            creating (and occasionally designing) exceptional digital
-            experiences that are fast, accessible, visually appealing, and
-            responsive. Even though I have been creating web applications for
-            over 7 years, I still love it as if it was something new.
+        <div className="order-1 mt-20">
+          <h2 className="text-4xl">Hi, I’m {name} 👋</h2>
+          <p className="mt-2 text-base text-slate-500">
+            I'm a full stack developer (React.js & Node.js) with a focus on creating (and occasionally designing) exceptional digital experiences that are fast, accessible, visually appealing, and responsive.
+            Even though I have been creating web applications for over 7 years, I still love it as if it was something new.
           </p>
         </div>
       </div>
