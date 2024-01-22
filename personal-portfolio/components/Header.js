5f2332b60b841ec68cdc6146461d5e744050eeab
erdenetsogta <a.erdenetsogt@gmail.com>
2024-01-22 17:06:32 +0800

    mobile menu, state

--- personal-portfolio/components/Header.js
+++ personal-portfolio/components/Header.js
@@ -1,17 +1,14 @@
 import { menuItems } from "@/data/menuItems";
 import { MainMenu } from "./MainMenu";
 import { Logo } from "./icons/Logo";
-import { Menu } from "./icons/Menu";
 
 export function Header() {
   return (
     <div className="flex justify-between p-4 border-2">
       <Logo />
-      <div className="sm:flex md:hidden">
-        <Menu />
-      </div>
+
       <MainMenu items={menuItems} />
-      <a href="cv.jpg" className="px-4 py-2 text-white bg-black rounded hover:bg-slate-500" download>
+      <a href="cv.jpg" className="hidden px-4 py-2 text-white bg-black rounded md:block hover:bg-slate-500" download>
         Download
       </a>
     </div>
--- personal-portfolio/components/MainMenu.jsx
+++ personal-portfolio/components/MainMenu.jsx
@@ -1,11 +1,47 @@
+import { useState } from "react";
+import { Menu } from "./icons/Menu";
+
 export function MainMenu({ items }) {
   return (
-    <div className="hidden sm:flex">
-      {items.map((item) => (
-        <a key={item.id} className="px-4 py-2" href={item.link}>
-          {item.label}
-        </a>
-      ))}
-    </div>
+    <>
+      <div className="hidden md:flex">
+        {items.map((item) => (
+          <a key={item.id} className="px-4 py-2" href={item.link}>
+            {item.label}
+          </a>
+        ))}
+      </div>
+      <MobileMenu />
+    </>
+  );
+}
+
+function MobileMenu() {
+  const [visible, setVisible] = useState(false);
+  // const [name, setName] = useState("Bold");
+
+  // let open = false;
+
+  function openMenu() {
+    //   open = true;
+    setVisible(true);
+  }
+
+  function closeMenu() {
+    //   open = false;
+    setVisible(false);
+  }
+
+  return (
+    <>
+      <button className="sm:flex md:hidden" onClick={openMenu}>
+        <Menu />
+      </button>
+      {visible && <div className="fixed inset-0 bg-slate-400/70"></div>}
+      <div className={`fixed top-0 bottom-0 w-3/4 transition-all bg-white ${visible ? "right-0" : "-right-full"}`}>
+        <div className="p-8 text-2xl">Mobile Menu</div>
+        <button onClick={closeMenu}>close</button>
+      </div>
+    </>
   );
 }
